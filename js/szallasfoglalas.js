081bedd1051087c8a1e3b046efd80197cf52a398
Kurucz Tímea <79456559+ktimi30@users.noreply.github.com>
2021-06-03 16:02:58 +0200

    Update szallasfoglalas.js

--- js/szallasfoglalas.js
+++ js/szallasfoglalas.js
@@ -9,6 +9,14 @@ function init() {
 }
 
 function foglalas() {
+     ures($("nev"));
+    ures($("email"));
+    ures($("szam"));
+    ures($("erkezes"));
+    ures($("tavozas"));
+    ures($("felnottek"));
+    ures($("gyerekek"));
+    ures($("szoba"));
     if ($("szam") >= 10) {
         window.alert("Hibás telefonszámot adott meg!");
     }
@@ -22,4 +30,10 @@ function foglalas() {
         window.alert("Hibás kódot adott meg!");
     }
     $("foglalas").innerHTML = "Kedves " + $("nev").value + "! A szobafoglalás sikeresen megtörtént!";
+}
+function ures(ertek){
+if(ertek.value.length == 0){
+    window.alert("A mezők kitöltése kötelező!");
+    $("form").reset();
+    $("btn").removeEventListener();
 }
